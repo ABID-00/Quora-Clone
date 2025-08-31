8f29c3322dad059e7f2f442a838a9ec439049ef5
ABID'S PC <abidshaikh7478@gmail.com>
2025-08-31 12:31:21 +0530

    added new Project files

---
+++ Readme.md
@@ -0,0 +1,2 @@
+#new Project
+Created From system
---
+++ index.js
@@ -0,0 +1,86 @@
+const express = require(`express`);
+const app = express();
+const port= 4090;
+const path= require("path");
+const {v4:uuidv4}=require(`uuid`);
+const methodOverride = require("method-override");
+
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({extended:true}));
+// app.use(express.json());
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname,"views"));
+app.use(express.static(path.join(__dirname,"public")));
+
+let posts=[
+    {
+        username:`abid`,
+        id:uuidv4(),
+        content:`this is my 1st post`
+    },
+    {
+        username:`user2`,
+        id:uuidv4(),
+        content:`this quora's 1st post`
+    }
+];
+
+//create posts
+app.get("/posts/new",(req,res)=>{
+    res.render("new.ejs");
+})
+
+
+//display created post in main 
+app.post("/posts",(req,res)=>{
+    let {username,content}= req.body;
+    let id =uuidv4();
+    posts.push({username, id ,content});
+    res.redirect("/posts");
+}) 
+
+//show posts
+app.get("/posts/:id",(req,res)=>{
+    let {id} = req.params;
+    let post = posts.find((p)=> id === p.id);
+    res.render("show.ejs",{post});
+})
+
+
+app.patch("/posts/:id",(req,res)=>{
+    let {id} = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p)=> id === p.id); 
+    post.content= newContent;
+    console.log(post);
+    res.redirect("/posts");
+});
+
+app.delete("/posts/:id",(req,res)=>{
+    let {id} = req.params;
+    posts= posts.filter((p)=> id !== p.id); 
+    res.send("delete sucess");
+
+})
+
+app.get("/posts/:id/edit",(req,res)=>{
+    let {id} = req.params;
+    let post = posts.find((p)=> id === p.id); 
+    res.render("edit.ejs",{post});
+});
+
+//
+app.get("/posts",(req,res)=>{
+    res.render("index.ejs",{posts});
+});
+
+app.listen(port,()=>{
+    console.log(`listening to port ${port}`);
+})
+
+
+app.get("/",(req,res)=>{
+    res.send(`Server is Enabled`);
+});
